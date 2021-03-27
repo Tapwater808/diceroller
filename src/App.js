f5ceb178f378069600d80439099c71926e57d72c
Mason M <m8mamiya@gmail.com>
2021-03-26 21:46:16 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+
+  state = {
+    numberOfDice: null,
+    rolls: [],
+    rollSum: null
+  };
+  diceRoll = numberOfDice => {
+    let rolls = [];
+    let rollSum = 0;
+    for (let i = 0; i < numberOfDice; i++) {
+      rolls[i] = Math.floor(Math.random() * 6) + 1;
+      rollSum += rolls[i];
+    }
+    this.setState({
+      numberOfDice,
+      rolls,
+      rollSum
+    });
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Dice Roll Demo</h1>
+        {/* Buttons */}
+        <div className="buttons">
+  {[1, 2, 3, 4, 5].map(number => {
+    let text = number === 1 ? "die" : "dice";
+    return (
+      <button
+        key={number}
+        onClick={() => this.diceRoll(number)}
+        className="button"
+      >
+        {number} {text}
+      </button>
+    );
+  })}
+</div>
+      </div>
+    );
+  }
+}
+export default App;
